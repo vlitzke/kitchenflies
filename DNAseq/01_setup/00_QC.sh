adc9f0c76fd93f548bab0df2896a968e65b2dfd6
vlitzke <45816606+vlitzke@users.noreply.github.com>
2024-01-31 12:26:46 +0000

    Add files via upload

---
+++ DNAseq/01_setup/00_QC.sh
@@ -0,0 +1,22 @@
+#!/bin/bash			
+
+#SBATCH --job-name="00_QC"		# job name
+#SBATCH --ntasks=4                    # Run on a single CPU
+#SBATCH --mem=8G                     # Job memory request
+#SBATCH --partition=short	  # ~how long a job will take
+#SBATCH --output=00_QC.log   # Standard output and error log
+
+pwd; hostname; date
+
+echo "Starting job on $HOSTNAME"
+echo [`date +"%Y-%m-%d %H:%M:%S"`]
+
+eval "$(conda shell.bash hook)"
+conda activate dnaseq
+
+cd ./PATH/TO/RawData
+
+fastqc *.fq.gz
+
+echo "Job finished"
+
